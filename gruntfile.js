649859e5e67d072b77d21de99982c42648f505ab
nicolas philippe <nicolas.philippe33@gmail.com>
2013-07-14 22:16:17 +0200

    Change Watch params for subfolders

--- gruntfile.js
+++ gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		watch: {
 			livereload: {
-				files: 'app/*',
+				files: 'app/**/*',
 				options: {
 					livereload: LIVERELOAD_PORT,
 				},
